c8e317a01ae787b5e19d629b3b7a6a2aeb1752aa
jose luis <cortezluisjose@gmail.com>
2019-06-11 02:28:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // write your code below!
 
 var name = "Jose"
-var height = "74"
+var height = 74;
 
 console.log("Name:")
 console.log(name)
@@ -9,3 +9,4 @@ console.log(name)
 console.log("Height:")
 console.log(height)
 console.log(name)
+console.log(height + 1)
